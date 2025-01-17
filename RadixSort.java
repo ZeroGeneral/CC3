3e46b1c1824844bd1c12f129fd29974164c9747b
ZeroGeneral <169157020+ZeroGeneral@users.noreply.github.com>
2025-01-18 00:55:11 +0800

    Update RadixSort.java

--- RadixSort.java
+++ RadixSort.java
@@ -4,19 +4,22 @@
  * and open the template in the editor.
  */
 
-package radixsort;
+package radixtrials;
 
 /**
  *
  * @author clyde
  */
+
 import java.util.Arrays;
 
-public class RadixSort{
+public class RadixTrials {
+
+    private static int counter = 0; // Global counter variable
+
     public static void main(String[] args) {
         int[] arr = {10, 7, 8, 9, 1};
         int n = arr.length;
-        int counter = 0; // Counter variable
 
         // Find the maximum number to know the number of digits
         int max = getMax(arr, n);
@@ -24,53 +27,62 @@ public class RadixSort{
 
         // Do counting sort for every digit
         for (int exp = 1; max / exp > 0; exp *= 10) {
-            counter++;
-            countingSort(arr, n, exp, counter);
+            counter++; // For each loop iteration
+            countingSort(arr, n, exp);
         }
 
         // Print sorted array and counter value
         System.out.println("Sorted array: " + Arrays.toString(arr));
-        System.out.println("Instruction Count: " + counter);
+        System.out.println("Instruction Count (TFC): " + counter);
     }
 
     static int getMax(int[] arr, int n) {
         int max = arr[0];
+        counter++; // For initialization
         for (int i = 1; i < n; i++) {
+            counter++; // Loop comparison
             if (arr[i] > max) {
                 max = arr[i];
+                counter++; // For assignment
             }
         }
+        counter++; // Last loop check
         return max;
     }
 
-    static void countingSort(int[] arr, int n, int exp, int counter) {
+    static void countingSort(int[] arr, int n, int exp) {
         int[] output = new int[n]; // Output array
         int[] count = new int[10];
-        counter += 2; // For output and count array initialization
+        counter += 2; // For array initializations
 
         // Store count of occurrences in count[]
         for (int i = 0; i < n; i++) {
             count[(arr[i] / exp) % 10]++;
-            counter++;
+            counter += 2; // For array access and increment
         }
+        counter++; // Last loop check
 
         // Change count[i] so that count[i] contains actual position of this digit
         for (int i = 1; i < 10; i++) {
             count[i] += count[i - 1];
-            counter++;
+            counter++; // For addition
         }
+        counter++; // Last loop check
 
         // Build the output array
         for (int i = n - 1; i >= 0; i--) {
             output[count[(arr[i] / exp) % 10] - 1] = arr[i];
             count[(arr[i] / exp) % 10]--;
-            counter += 2;
+            counter += 3; // For array access, decrement, and assignment
         }
+        counter++; // Last loop check
 
         // Copy the output array to arr[]
         for (int i = 0; i < n; i++) {
             arr[i] = output[i];
-            counter++;
+            counter++; // For assignment
         }
+        counter++; // Last loop check
     }
 }
+
